23e8e04a74657cb5ce0af6919c35d29a76ece5d2
Anurag Deshmukh <111648310+anurag0134@users.noreply.github.com>
2023-09-20 05:20:49 +0000

    datatypes in javascript

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+"use strict"; // Treat all JS code as newer version
+
+//alert( 3 + 3) // we are using node js not browser
+
+console.log(3
+    +
+    3) // code readability should be high
+
+console.log("Anurag")
+
+let name = "Anurag"
+let age = 18 
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof "Anurag");
+console.log(typeof age)
+console.log(typeof null) //tyoe of null is object
+console.log(typeof undefined)
